f5d28f19b3ecd371759f3c830749619bc878e660
waelmahdy <wael_mahdy@yahoo.com>
2024-04-29 11:48:52 +0100

    migratttt

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT=BASE_DIR /'static'
 STATICFILES_DIRS=[
     'greatkart/static',
